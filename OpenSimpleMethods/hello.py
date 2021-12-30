29160be712bc1f571ba3a5ced6269803aa999909
Andrew Voronin <46493509+agrobnarv@users.noreply.github.com>
2021-12-30 22:03:01 +0300

    update Hello test

--- OpenSimpleMethods/hello.py
+++ OpenSimpleMethods/hello.py
@@ -1,4 +1,29 @@
+import pytest
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/hello", params={"name": "Andrew"})
-print(response.text)
+
+class TestHello:
+    names = [
+        ("Andrew"),
+        ("Ivan"),
+        ("")
+    ]
+
+    @pytest.mark.parametrize('name', names)
+    def test_hello_api(self, name):
+        url = "https://playground.learnqa.ru/api/hello"
+        data = {"name": name}
+
+        response = requests.get(url, params=data)
+        assert response.status_code == 200, "Неверный статус код"
+
+        response_dict = response.json()
+        assert "answer" in response_dict, "Нет поля 'answer' в ответе"
+
+        if len(name) == 0:
+            expected_response_text = "Hello, someone"
+        else:
+            expected_response_text = f"Hello, {name}"
+
+        actual_response_text = response_dict["answer"]
+        assert actual_response_text == expected_response_text, "Некорректный текст в ответе"
